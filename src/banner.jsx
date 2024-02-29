ec8c2f49e84a03a207ab3ec39076247197a72201
sojinkshibu12 <sojinkshibu@gmail.com>
2024-02-29 19:52:35 +0530

    third

--- src/banner.jsx
+++ src/banner.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
-import React from 'react';
+import React ,{useEffect ,useState} from 'react';
 import { useScroll, useTransform, motion} from 'framer-motion';
 import { useRef } from 'react';
+
+
 
 const Banner =  ()=>{
 
@@ -14,18 +16,22 @@ const Banner =  ()=>{
       offset: ['start start', 'end end']
 
   })
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  const scale2 = useTransform(scrollYProgress,[0,1],[0,3]);
+  const opscale = useTransform(scrollYProgress, [0, 1], [1.5,0]);
 
-
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 2]);
-  const scale2 = useTransform(scrollYProgress,[0,1],[0,1.5]);
     return(
-      <div ref  = {container} className="container">
+      
+      <div  ref  = {container } className="container" >
           <div className="sticky">
-            <div className="imagecontainer">
+
+            <motion.div  style = {{opacity : opscale}} className="imagecontainer" >
               <motion.img style = {{opacity : scale2}} className = "logo"src="bgg.png" alt="logo" />
                 <img className = "img1"src="city.jpeg" alt="city" />
                 <motion.img style = {{scale : scale}}className = "img2"src="leaf1.png" alt="city" />
-            </div>
+            </motion.div>
+
+            
           </div>
       </div> 
     );

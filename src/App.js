10962ffb63d4b97dbdd0ae7e2b9f57e4331b765c
Joel <102653084+Joelt112@users.noreply.github.com>
2024-03-01 13:05:38 +0530

    cha

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import Banner from "./banner"
 import Ghoonies  from "./ghoonies";
@@ -12,16 +11,5 @@ export default function App() {
       <Ghoonies/>
       
     </div>
-=======
-import "./App.css";
-import Banner from "./banner"
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-
-export default function App() {
-  return (
-    <Banner/>
->>>>>>> d8e0aeb247ffdd447b47faa7e201ccfcba4fe82d
   );
 }
--- src/banner.jsx
+++ src/banner.jsx
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import "./App.css";
 import React ,{useEffect ,useState} from 'react';
 import { useScroll, useTransform, motion} from 'framer-motion';
 import { useRef } from 'react';
-=======
-import Plx from "react-plx";
-import React from 'react';
-
->>>>>>> d8e0aeb247ffdd447b47faa7e201ccfcba4fe82d
 
 
 
 const Banner =  ()=>{
-<<<<<<< HEAD
 
   const container = useRef(null);
 
@@ -47,110 +40,6 @@ const Banner =  ()=>{
             
           </div>
       </div> 
-=======
-    return(
-        <div>
-      <Plx
-        parallaxData={[
-          {
-            start: 0,
-            end: 2000,
-            easing: "ease-in",
-            properties: [
-              {
-                startValue: 1,
-                endValue: 15,
-                property: "scale"
-              }
-            ]
-          }
-        ]}
-        style={{
-          position: "fixed",
-          left: 0,
-          top: 0,
-          width: "100%",
-          zIndex: 100
-        }}
-      >
-        <img style={{ width: "100%" }} src="bg.png" alt="foreground" />
-      </Plx>
-      <Plx
-        parallaxData={[
-          {
-            start: 0,
-            end: 800,
-            properties: [
-              {
-                startValue: 1,
-                endValue: 1.18,
-                property: "scale"
-              }
-            ]
-          }
-        ]}
-        style={{  
-          position: "fixed",
-          left: 0,
-          top: 0,
-          width: "100%"
-        }}
-      >
-    <img style={{ width: "100%" }} src="city.jpeg" alt="background" />
-      </Plx>
-      <Plx
-        
-        parallaxData={[
-          {
-            start: 0,
-            end: 400,
-            properties: [
-              {
-                startValue: 0,
-                endValue: 1,
-                property: "opacity"
-              }
-            ]
-          }
-        ]}
-        style={{
-          position: "fixed",
-          left: 0,
-          top: "100px",
-          marginLeft:"23%",
-          
-          width: "50%"
-        }}
-      >
-        <img style={{ width: "100%" }} src="bgg.png" alt="background" />
-      </Plx>
-     
-
-      {/* <div
-        style={{
-          position: "fixed",
-          lefft: 0,
-          top: 0,
-          zIndex: 200,
-          paddingTop: "56%",
-          height: "400vh",
-          width: "100%"
-        }}
-      >
-        <div
-          style={{
-            background: "#000",
-            height: "100%"
-          }}
-        ></div>
-      </div> */}
-
-     
-
-    </div>
-
- 
->>>>>>> d8e0aeb247ffdd447b47faa7e201ccfcba4fe82d
     );
     
 }
